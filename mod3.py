c16493ad86c7e36edcdc28829e7f707a3884f73f
tassyirifiyya <rifiyya20@mhs.unsyiah.ac.id>
2023-03-08 22:31:26 +0700

    Sesi 3,4,5 and gitignore

---
+++ mod3.py
@@ -0,0 +1,8 @@
+s = 'Hacktiv8-PTP Python for Data Science'
+a = [100,20,300]
+
+def foo(arg):
+    print('arg = '+arg)
+
+class Foo:
+    pass
---
+++ pkg/mod.py
@@ -0,0 +1,11 @@
+s = 'Hacktiv8-PTP Python for Data Science'
+a = [100,20,300]
+
+def foo(arg):
+    print('arg = '+arg)
+
+class Foo:
+    pass
+
+if(__name__ == '__main__'):
+    print('Hello World!)
